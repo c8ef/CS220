36efe2b047d5092a799a8f5c8c00dadcc3472245
c8ef <c8ef@outlook.com>
2022-09-11 11:54:27 +0800

    pass grade-2

--- src/assignments/assignment02.rs
+++ src/assignments/assignment02.rs
@@ -6,43 +6,61 @@
 //! You should fill out the `todo!()` placeholders in such a way that `/scripts/grade-02.sh` works fine.
 //! See `assignment02_grade.rs` and `/scripts/grade-02.sh` for the test script.
 
-use std::ops::Mul;
+use std::{ops::Mul, vec};
 
 const FAHRENHEIT_OFFSET: f64 = 32.0;
 const FAHRENHEIT_SCALE: f64 = 5.0 / 9.0;
 
 /// Converts Fahrenheit to Celsius temperature degree.
 pub fn fahrenheit_to_celsius(degree: f64) -> f64 {
-    todo!()
+    5_f64 / 9_f64 * (degree - 32_f64)
 }
 
 /// Capitalizes English alphabets (leaving the other characters intact).
 pub fn capitalize(input: String) -> String {
-    todo!()
+    input.to_ascii_uppercase()
 }
 
 /// Returns the sum of the given array. (We assume the absence of integer overflow.)
 pub fn sum_array(input: &[u64]) -> u64 {
-    todo!()
+    let mut ans: u64 = 0;
+    for &i in input {
+        ans += i;
+    }
+    ans
 }
 
 /// Given a non-negative integer, say `n`, return the smallest integer of the form `3^m` that's greater than or equal to `n`.
 ///
 /// For instance, up3(6) = 9, up3(9) = 9, up3(10) = 27. (We assume the absence of integer overflow.)
 pub fn up3(n: u64) -> u64 {
-    todo!()
+    let mut pow = 1;
+    while pow < n {
+        pow *= 3;
+    }
+    pow
 }
 
 /// Returns the greatest common divisor (GCD) of two non-negative integers. (We assume the absence of integer overflow.)
 pub fn gcd(lhs: u64, rhs: u64) -> u64 {
-    todo!()
+    if rhs == 0 {
+        return lhs;
+    }
+    gcd(rhs, lhs % rhs)
 }
 
 /// Returns the array of nC0, nC1, nC2, ..., nCn, where nCk = n! / (k! * (n-k)!). (We assume the absence of integer overflow.)
 ///
 /// Consult <https://en.wikipedia.org/wiki/Pascal%27s_triangle> for computation of binomial coefficients without integer overflow.
 pub fn chooses(n: u64) -> Vec<u64> {
-    todo!()
+    let mut v = vec![1_u64];
+
+    let mut last = 1_u128;
+    for i in 1..=n {
+        last = last * (n + 1 - i) as u128 / i as u128;
+        v.push(last as u64)
+    }
+    v
 }
 
 /// Returns the "zip" of two vectors.
@@ -50,7 +68,9 @@ pub fn chooses(n: u64) -> Vec<u64> {
 /// For instance, `zip(vec![1, 2, 3], vec![4, 5])` equals to `vec![(1, 4), (2, 5)]`.
 /// Here, `3` is ignored because it doesn't have a partner.
 pub fn zip(lhs: Vec<u64>, rhs: Vec<u64>) -> Vec<(u64, u64)> {
-    todo!()
+    lhs.into_iter()
+        .zip(rhs.into_iter())
+        .collect::<Vec<(u64, u64)>>()
 }
 
 /// 2x2 matrix of the following configuration:
@@ -91,7 +111,12 @@ impl Mul<Mat2> for Mat2 {
     type Output = Mat2;
 
     fn mul(self, rhs: Mat2) -> Self::Output {
-        todo!()
+        Mat2 {
+            a: (self.a * rhs.a).wrapping_add(self.b * rhs.c),
+            b: (self.a * rhs.b).wrapping_add(self.b * rhs.d),
+            c: (self.c * rhs.a).wrapping_add(self.d * rhs.c),
+            d: (self.c * rhs.b).wrapping_add(self.d * rhs.d),
+        }
     }
 }
 
@@ -99,21 +124,29 @@ impl Mul<Vec2> for Mat2 {
     type Output = Vec2;
 
     fn mul(self, rhs: Vec2) -> Self::Output {
-        todo!()
+        Vec2 {
+            a: self.a * rhs.a + self.b * rhs.b,
+            b: self.c * rhs.a + self.d * rhs.b,
+        }
     }
 }
 
 impl Mat2 {
     /// Calculates the power of matrix.
     fn power(self, power: u64) -> Mat2 {
-        todo!()
+        let mat = self;
+        let mut ans = self;
+        for i in 0..power {
+            ans = ans * mat;
+        }
+        ans
     }
 }
 
 impl Vec2 {
     /// Gets the upper value of vector.
     fn get_upper(self) -> u64 {
-        todo!()
+        self.b
     }
 }
 
@@ -139,5 +172,109 @@ pub fn fibonacci(n: u64) -> u64 {
 ///
 /// Hint: Google the song title for lyrics and look at the test code for the expected result.
 pub fn twelve_days_of_christmas_lyrics() -> String {
-    todo!()
+    let str = r#"On the first day of Christmas, my true love sent to me
+A partridge in a pear tree
+
+On the second day of Christmas, my true love sent to me
+Two turtledoves
+And a partridge in a pear tree
+
+On the third day of Christmas, my true love sent to me
+Three French hens
+Two turtledoves
+And a partridge in a pear tree
+
+On the fourth day of Christmas, my true love sent to me
+Four calling birds
+Three French hens
+Two turtledoves
+And a partridge in a pear tree
+
+On the fifth day of Christmas, my true love sent to me
+Five gold rings (five golden rings)
+Four calling birds
+Three French hens
+Two turtledoves
+And a partridge in a pear tree
+
+On the sixth day of Christmas, my true love sent to me
+Six geese a-laying
+Five gold rings (five golden rings)
+Four calling birds
+Three French hens
+Two turtledoves
+And a partridge in a pear tree
+
+On the seventh day of Christmas, my true love sent to me
+Seven swans a-swimming
+Six geese a-laying
+Five gold rings (five golden rings)
+Four calling birds
+Three French hens
+Two turtledoves
+And a partridge in a pear tree
+
+On the eighth day of Christmas, my true love sent to me
+Eight maids a-milking
+Seven swans a-swimming
+Six geese a-laying
+Five gold rings (five golden rings)
+Four calling birds
+Three French hens
+Two turtledoves
+And a partridge in a pear tree
+
+On the ninth day of Christmas, my true love sent to me
+Nine ladies dancing
+Eight maids a-milking
+Seven swans a-swimming
+Six geese a-laying
+Five gold rings (five golden rings)
+Four calling birds
+Three French hens
+Two turtledoves
+And a partridge in a pear tree
+
+On the tenth day of Christmas, my true love sent to me
+Ten lords a-leaping
+Nine ladies dancing
+Eight maids a-milking
+Seven swans a-swimming
+Six geese a-laying
+Five gold rings (five golden rings)
+Four calling birds
+Three French hens
+Two turtledoves
+And a partridge in a pear tree
+
+On the eleventh day of Christmas, my true love sent to me
+I sent eleven pipers piping
+Ten lords a-leaping
+Nine ladies dancing
+Eight maids a-milking
+Seven swans a-swimming
+Six geese a-laying
+Five gold rings (five golden rings)
+Four calling birds
+Three French hens
+Two turtledoves
+And a partridge in a pear tree
+
+On the twelfth day of Christmas, my true love sent to me
+Twelve drummers drumming
+Eleven pipers piping
+Ten lords a-leaping
+Nine ladies dancing
+Eight maids a-milking
+Seven swans a-swimming
+Six geese a-laying
+Five gold rings (five golden rings)
+Four calling birds
+Three French hens
+Two turtledoves
+And a partridge in a pear tree
+
+And a partridge in a pear tree
+"#;
+    str.to_string()
 }
