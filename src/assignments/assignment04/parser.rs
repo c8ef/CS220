7a7257e79921dbe88ee91734a739263781498db0
c8ef <c8ef@outlook.com>
2022-09-16 21:15:05 +0800

    finish parsing

--- src/assignments/assignment04/parser.rs
+++ src/assignments/assignment04/parser.rs
@@ -1,7 +1,13 @@
 //! Parser.
 
+use self::inner::*;
 use super::syntax::*;
 use anyhow::Result;
+use pest::{
+    iterators::{Pair, Pairs},
+    prec_climber::{Assoc, Operator, PrecClimber},
+    Parser,
+};
 
 #[allow(missing_docs)]
 #[allow(missing_debug_implementations)]
@@ -21,5 +27,60 @@ mod inner {
 ///
 /// e.g. `1+2+3` should be parsed into `(1+2)+3`, not `1+(2+3)` because the associativity of plus("add" in our hw) operator is `Left`.
 pub fn parse_command(line: &str) -> Result<Command> {
-    todo!("fill here")
+    let pairs = SyntaxParser::parse(Rule::command, line)?;
+    match pairs.peek().unwrap().as_rule() {
+        Rule::expr => Ok(Command {
+            variable: None,
+            expression: turn_pairs_into_expressions(pairs.peek().unwrap().into_inner()),
+        }),
+        _ => Ok(Command {
+            variable: Some(pairs.peek().unwrap().as_str().to_string()),
+            expression: turn_pairs_into_expressions(pairs.clone().nth(1).unwrap().into_inner()),
+        }),
+    }
+}
+
+fn turn_pairs_into_expressions(line: Pairs<'_, Rule>) -> Expression {
+    let climber = PrecClimber::new(vec![
+        Operator::new(Rule::add, Assoc::Left) | Operator::new(Rule::subtract, Assoc::Left),
+        Operator::new(Rule::multiply, Assoc::Left) | Operator::new(Rule::divide, Assoc::Left),
+        Operator::new(Rule::power, Assoc::Right),
+    ]);
+    let infix = |lhs: Expression, op: Pair<'_, Rule>, rhs: Expression| match op.as_rule() {
+        Rule::add => Expression::BinOp {
+            op: BinOp::Add,
+            lhs: Box::new(lhs),
+            rhs: Box::new(rhs),
+        },
+        Rule::subtract => Expression::BinOp {
+            op: BinOp::Subtract,
+            lhs: Box::new(lhs),
+            rhs: Box::new(rhs),
+        },
+        Rule::multiply => Expression::BinOp {
+            op: BinOp::Multiply,
+            lhs: Box::new(lhs),
+            rhs: Box::new(rhs),
+        },
+        Rule::divide => Expression::BinOp {
+            op: BinOp::Divide,
+            lhs: Box::new(lhs),
+            rhs: Box::new(rhs),
+        },
+        Rule::power => Expression::BinOp {
+            op: BinOp::Power,
+            lhs: Box::new(lhs),
+            rhs: Box::new(rhs),
+        },
+        _ => unreachable!(),
+    };
+
+    let primary = |pair: Pair<'_, Rule>| match pair.as_rule() {
+        Rule::num => Expression::Num(pair.as_str().parse::<f64>().unwrap()),
+        Rule::var => Expression::Variable(pair.as_str().to_string()),
+        Rule::expr => turn_pairs_into_expressions(line.peek().unwrap().into_inner()),
+        _ => unreachable!(),
+    };
+
+    climber.climb(line.clone(), primary, infix)
 }
